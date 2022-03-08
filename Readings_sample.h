bfd5d4157dc004c5ae940fa748089c25887878f8
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-03-08 16:05:20 +0530

    Create Readings_sample.h

---
+++ Readings_sample.h
@@ -0,0 +1,4 @@
+const char* detectRange(int arr[], int readingscount);
+extern int getMinVal(int arr[]), int readingscount);
+extern int getMaxVal(int arr[]), int readingscount);
+extern int getreadingscount(int* arr);
