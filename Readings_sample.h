7f3c4fff28e04d57bfc61372a6a5aaac63eb2049
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-03-29 21:47:17 +0530

    Update Readings_sample.h

--- Readings_sample.h
+++ Readings_sample.h
@@ -1,4 +1,16 @@
-const char* detectRange(int arr[], int readingscount);
-extern int getMinVal(int arr[], int readingscount);
-extern int getMaxVal(int arr[], int readingscount);
-extern int getreadingscount(int arr[]);
+#pragma once
+
+#include <stdio.h>
+
+typedef struct 
+{
+int startVal;
+int endVal;
+int RangeofSamplesCount;
+}ChargingValueRange;
+
+extern int checkValidReadings(extern int *chargingValueArray, extern int readingsCount);
+extern int* sortReadings(extern int *chargingValueArray, extern int readingsCount);
+extern int cmpfunc (const void * val1, const void * val2);
+extern int countConsecutiveRange(extern int *consecutiveChargingValues, extern int valCount);
+ChargingValueRange displayRangesandReadings(extern int MinVal, extern int MaxVal, extern int samplesCount);
