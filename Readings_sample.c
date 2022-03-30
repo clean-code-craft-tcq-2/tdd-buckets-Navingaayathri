2753e4445567eda374887b7a461bb2c40423a83c
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-03-30 12:47:56 +0530

    Update Readings_sample.c

--- Readings_sample.c
+++ Readings_sample.c
@@ -1,7 +1,7 @@
 #include "Readings_sample.h"
 int checkValidReadings(int *chargingValueArray, int readingsCount)
 {
-  int* result = (int*) calloc (chargingValueArray, sizeof(readingsCount));
+  int* result = (int*) calloc (chargingValueArray, sizeof(int));
   for (int i = 0; i < readingsCount; i++) 
   {
    if (chargingValueArray[i]>=0) 
