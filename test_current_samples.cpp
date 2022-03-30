b8a556f6dca021a7751be64de63d72cae1bba363
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-03-30 15:20:01 +0530

    Update test_current_samples.cpp

--- test_current_samples.cpp
+++ test_current_samples.cpp
@@ -3,24 +3,32 @@
 #include "test-framework/catch.hpp"
 #include "Readings_sample.h"
 
-TEST_CASE("To check the input current readings are valid and find consecutive Charging Values") 
-{
-  int inputCurrentSamples[] = {4,5};
+TEST_CASE("To check the input current readings are valid and find consecutive Charging Values") {
+  int consecutiveChargingValues[] = {4,5};
   int valCount = sizeof(consecutiveChargingValues) / sizeof(consecutiveChargingValues[0]);
-  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == 1);
+  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == true);
   REQUIRE(countConsecutiveRange(consecutiveChargingValues, valCount) == 2);
 }
 
-TEST_CASE("To check the input current readings are invalid") 
-{
+TEST_CASE("To check the input current readings are invalid") {
   int consecutiveChargingValues[] = {-8, 2 ,-4, -2, -1, 3, 5, 6};
   int valCount = sizeof(consecutiveChargingValues) / sizeof(consecutiveChargingValues[0]);
-  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == 0);
+  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == false);
 }
 
-TEST_CASE("TO test the ranges are consecutive") 
-{
+TEST_CASE("To test the ranges are consecutive") {
   int consecutiveChargingValues[] = {3, 3, 5, 4, 10, 11, 12};
   int valCount = sizeof(consecutiveChargingValues) / sizeof(consecutiveChargingValues[0]);
-  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == 1);
+  REQUIRE(checkValidReadings(consecutiveChargingValues,valCount) == true);
+}
+
+TEST_CASE("Check the input range and count the ocuurences ") {
+  int consecutiveChargingValues[] = {1, 3, 3 ,5, 4, 6};
+  int valCount = sizeof(consecutiveChargingValues) / sizeof(consecutiveChargingValues[0]);
+  int outputStartVal = 1, outputEndVal = 6 , outputSamplesCount = 6;
+  ChargingValueRange chargingValueRange;
+  chargingValueRange = displayRangesandReadings(outputStartVal,outputEndVal,outputSamplesCount);
+  REQUIRE(rangeReading.startVal == outputStartVal);
+  REQUIRE(rangeReading.endVal == outputEndVal);
+  REQUIRE(rangeReading.RangeofSamplesCount == outputSamplesCount);
 }
