1987314bb6e7715f05e09853ee66b1df0ecddccc
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-03 10:31:13 +0530

    Update test_current_samples.cpp

--- test_current_samples.cpp
+++ test_current_samples.cpp
@@ -4,33 +4,32 @@
 #include "Readings_sample.h"
 
 
-Scenario : To sort the array of current samples
+Scenario "To sort the array of current samples"
 {
-    Given : An Unsorted array of current samples
+    Given "An Unsorted array of current samples"
 		int inpCurrentSamples[11] = {4, 7, 1, 3, 2, 7, 5, 6, 9, 10, 50};
         int expCurrentSamples[11] = {1, 2, 3, 4, 5, 6, 7, 7, 9, 10, 50};
 
-    When : sortReadings() function is called with current samples array and count
+    When "sortReadings() function is called with current samples array and count"
             int(*realPtr) = sortReadings(inpCurrentSamples, 11);
-	Then : Gives the expected sorted array
+	Then "Gives the expected sorted array"
                 for (int i = 0; i < 10; i++)
                 {
                     CHECK(realPtr[i] == expCurrentSamples[i]);
                 }
 
 
-Scenario : To find the ranges and occurrences in the sorted array of current samples
+Scenario To find the ranges and occurrences in the sorted array of current samples
 {
-    Given :Sorted array of current samples
+    Given "Sorted array of current samples"
         int inpCurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
         chargingValueRange expOutput[2] = {{3, 5, 4}, {10, 12, 3}};
         RangeofSamples expRangeofSamplesCount = {expOutput, 2};
 
-    When : findchargingValueRange() is called with Charging values array and readingsCount
+    When "findchargingValueRange() is called with Charging values array and readingsCount"
         RangeofSamples realRangeofSamples = findchargingValueRange(inpCurrentSamples, 7);
             
-	Then : 
-
+    Then "Ranges and count of samples in the range will be displayed as the output"
     CHECK(realRangeofSamplesCount.RangeofSamplesCount == expRangeofSamplesCount.RangeofSamplesCount);
 
                 int isEqual = 1;
