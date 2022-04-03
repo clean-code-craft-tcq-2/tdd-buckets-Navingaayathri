2c8b9648311a3deb0ec4d7dba530dcd07356b796
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-03 13:19:10 +0530

    Update test_current_samples.cpp

--- test_current_samples.cpp
+++ test_current_samples.cpp
@@ -8,8 +8,8 @@ SCENARIO("To sort the array of current samples")
 {
     GIVEN ("An Unsorted array of current samples"){
 
-        int inpCurrentSamples[10] = {1, 3, 3, 16, 4, 10, 11, 12, 5, 17};
-        int expCurrentSamples[10] = {1, 3, 3, 4, 5, 10, 11, 12, 16, 17};
+        int inpCurrentSamples[11] = {1, 3, 3, 16, 2, 4, 10, 11, 12, 5, 17};
+        int expCurrentSamples[11] = {1, 2, 3, 3, 4, 5, 10, 11, 12, 16, 17};
 
     WHEN ("sortReadings() function is called with current samples array and count"){
             int(*realPtr) = sortReadings(inpCurrentSamples, 10);
@@ -26,12 +26,12 @@ SCENARIO("To sort the array of current samples")
 SCENARIO("To find the ranges and occurrences in the sorted array of current samples")
 {
     GIVEN ("Sorted array of current samples"){
-        int inpCurrentSamples[10] = {1, 3, 3, 16, 4, 10, 11, 12, 5, 17};
-        chargingValueRange expOutput[3] = {{1, 5, 5}, {10, 12, 3}, {16, 17, 2}};
+        int inpCurrentSamples[11] = {1, 2, 3, 3, 16, 4, 10, 11, 12, 5, 17};
+        chargingValueRange expOutput[3] = {{1, 5, 6}, {10, 12, 3}, {16, 17, 2}};
         RangeofSamples expRangeofSamplesCount = {expOutput, 3};
 
     WHEN ("findchargingValueRange() is called with Charging values array and readingsCount"){
-        RangeofSamples realRangeofSamples = findchargingValueRange(inpCurrentSamples, 10);
+        RangeofSamples realRangeofSamples = findchargingValueRange(inpCurrentSamples, 11);
             
     THEN ("Ranges and count of samples in the range will be displayed as the output"){
          REQUIRE(realRangeofSamples.RangeofSamplesCount == expRangeofSamplesCount.RangeofSamplesCount);
@@ -49,8 +49,8 @@ SCENARIO("To find the ranges and occurrences in the sorted array of current samp
  
 TEST_CASE("To test displayRangesandReadings function for printing the output")
 {
-    int inpCurrentSamples[10] = {1, 3, 3, 16, 4, 10, 11, 12, 5, 17};
-    chargingValueRange chargingValues[3] = {{1, 5, 5}, {10, 12, 3}, {16, 17, 2}};
+    int inpCurrentSamples[11] = {1, 3, 3, 16, 2, 4, 10, 11, 12, 5, 17};
+    chargingValueRange chargingValues[3] = {{1, 5, 6}, {10, 12, 3}, {16, 17, 2}};
     RangeofSamples samplesRange = {chargingValues, 3};
     REQUIRE_NOTHROW(displayRangesandReadings(samplesRange));
 }
