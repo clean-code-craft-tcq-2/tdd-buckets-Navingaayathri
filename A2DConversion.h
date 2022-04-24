e4eae4215882106265180b7d298db3f19340ec54
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-24 09:32:38 +0530

    TDD extension

---
+++ A2DConversion.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int findInternalMaxSens(int ADCFactor);
+float executeConversion(int ADCInput, int ADCFactor, int currValMax);
+int convertADCinpToPhyValSens(int ADCInput[], int ADCFactor, int ADCInpSize, int *currValPhy, int currValMax);
+int convertADCinpToPhyValSens2(int ADCInput[], int ADCFactor, int ADCInpSize, int *currValPhy, int currValMax);
