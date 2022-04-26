203e8c7c6338c6afc711fd88eb807cc3ca3fbbb8
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-26 18:46:39 +0530

    Update A2DConversion.h

--- A2DConversion.h
+++ A2DConversion.h
@@ -5,4 +5,4 @@
 int findInternalMaxSens(int ADCFactor);
 float executeConversion(int ADCInput, int ADCFactor, int currValMax);
 void convertADCinpToPhyValSens(int ADCInput[], int ADCFactor, int ADCInpSize, int* currValPhy, int currValMax);
-void convertADCinpToPhyValSens2(int ADCInput[], int ADCFactor, int ADCInpSize, int* currValPhy, int currValMax);
+int* convertADCinpToPhyValSens2(int ADCInput[], int ADCFactor, int ADCInpSize, int* currValPhy, int currValMax);
