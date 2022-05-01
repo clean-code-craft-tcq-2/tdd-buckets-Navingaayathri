e3e7a42f099eb5bc77f25efc1729bf65c21d4d4b
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-05-01 19:04:51 +0530

    Update test_current_samples.cpp

--- test_current_samples.cpp
+++ test_current_samples.cpp
@@ -60,15 +60,15 @@ TEST_CASE("To test displayRangesandReadings function for printing the output")
 
 SCENARIO("Test the conversion of 12 bit ADC values to real current values(0-10A)") {
 GIVEN("Internal raw values from ADC sensor min:0, max:2^12-->4094") {
-    int ADCInpArr[6] = {0, 560, 1250, 2500, 3250, 3800};
-    int expOutput[6] = {0,1,3,6,8,9};
+    int ADCInpArr[7] = {0, 560, 1250, 2500, 3250, 3800,4094};
+    int expOutput[7] = {0,1,3,6,8,9,10};
     int ADCInpSize = sizeof(ADCInpArr)/sizeof(ADCInpArr[0]);
     int ADCFactor =12, currValMax = 10, currValPhy[ADCInpSize];
 
 WHEN("Internal value to Physical Value conversion function conversion InternalToPhysical() is called with input array, Resolution and Physical range") {
  int* realOutput = convertADCinpToPhyValSens2(ADCInpArr, ADCFactor, ADCInpSize, currValPhy, currValMax);
 THEN("The Physical value array is returned for the given internal array input ") {
-   for(int i=0;i<ADCInpSize-1;i++)
+   for(int i=0;i<ADCInpSize;i++)
     {
 //REQUIRE(expcurrValMax ==currValMax);
 //REQUIRE(expADCFactor == ADCFactor);
