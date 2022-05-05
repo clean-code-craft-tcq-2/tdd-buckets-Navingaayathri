1467ab651cfa0052269f059069baaf6b4c38df50
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-05-05 23:20:27 +0530

    Update test_current_samples.cpp

--- test_current_samples.cpp
+++ test_current_samples.cpp
@@ -99,11 +99,11 @@ THEN("The Physical value array is returned for the given internal array input ")
 }*/
 TEST_CASE("Test the conversion of 10 bit ADC values to real current values(-15A to 15A)")
 {
- int ADCInput[] = {64, 1000, 1146, 1220, 1560, 2048, 4094};
+        int ADCInput[] = {300,550,1022};
 	int ADCInpSize = sizeof(ADCInput) / sizeof(ADCInput[0]);
-	int expConvertedValues[] = {0, 2, 2, 2, 3, 5, 10};
+	int expConvertedValues[] = {4,8,15};
 	int phyCurrVal[ADCInpSize];
-	int ADC_Factor = 12, currentValMax = 10;
+	int ADC_Factor = 10, currentValMax = 15;
 	phyValConversion(ADCInput, ADCInpSize, ADC_Factor, currentValMax, phyCurrVal);
 	for(int i = 0; i < ADCInpSize; ++i)
 	{
