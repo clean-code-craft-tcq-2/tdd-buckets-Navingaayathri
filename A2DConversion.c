34ab2e4d78089efa826b39603cd2278b0648739f
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-05-05 20:14:42 +0530

    Update A2DConversion.c

--- A2DConversion.c
+++ A2DConversion.c
@@ -1,11 +1,5 @@
 #include "A2DConversion.h"
 
-/*Error display function*/
-void displayError(void)
-{
-	printf("Error : Invalid value!\n");
-}
-
 /* Function to find the maximum internal raw value for the given resolution(factor) of ADC */
 int findInternalMaxSens(int ADC_FACTOR, int value)
 {
@@ -13,14 +7,14 @@ int findInternalMaxSens(int ADC_FACTOR, int value)
 }
 
 /* Conversion function to calculate physical value of current samples with maximum raw value of given resolution(factor) */
-void convertADCinpToPhyValSens(int ADCInput[], int ADCInpSize, int ADC_FACTOR, int CURRENTVALMAX, int* convertedVal)
+void convertADCinpToPhyValSens(int ADCInput[], int ADCInpSize, int ADC_Factor, int currentValMax, int* convertedVal)
 {
 	float currentValue;
 	int   adcSensVal;	
 	for (int i = 0; i < ADCInpSize ; ++i)
 	{
 		adcSensVal = (CURRENTVALMAX * ADCInput[i]);
-		currentValue    = (adcSensVal/(findInternalMaxSens(ADC_FACTOR,2)));
+		currentValue    = (adcSensVal/(findInternalMaxSens(ADC_Factor,2)));
 		convertedVal[i] = round(currentValue);
 		if(convertedVal[i] < 0)
 		{
@@ -30,17 +24,17 @@ void convertADCinpToPhyValSens(int ADCInput[], int ADCInpSize, int ADC_FACTOR, i
 }
 
 /* Input raw values from ADC to physical value conversion */
-void phyValConversion(int ADCInput[], int ADCInpSize, int ADC_FACTOR, int CURRENTVALMAX, int* phyCurrVal)
+void phyValConversion(int ADCInput[], int ADCInpSize, int ADC_Factor, int currentValMax, int* phyCurrVal)
 {
 	for (int i = 0; i< ADCInpSize; ++i)
 	{
 		if(ADCInput[i] < (findInternalMaxSens(ADC_FACTOR,1)))
 		{
-		convertADCinpToPhyValSens(ADCInput, ADCInpSize, ADC_FACTOR, CURRENTVALMAX, phyCurrVal);
+		convertADCinpToPhyValSens(ADCInput, ADCInpSize, ADC_Factor, currentValMax, phyCurrVal);
 		}
 		else
 		{
-		displayError();	
+		printf("Error : Invalid value!\n");
 		}
 	}
 }
